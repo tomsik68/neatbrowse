4d6723d11afc7efc80961b2316708e6e5891942e
tomsik68 <tomsik68@gmail.com>
2015-05-13 21:11:07 +0200

    Verbose comments for terminal.c

--- terminal.c
+++ terminal.c
@@ -1,15 +1,22 @@
 #include "terminal.h"
 
+
+/* struct that holds old terminal settings */
 static struct termios oldt;
 
 void terminalInit(){
     struct termios newt;
+    /* store old terminal settings */
     tcgetattr(STDIN_FILENO, &oldt);
+    /* copy old settings */
     newt = oldt;
+    /* remove ICANON and ECHO flags */
     newt.c_lflag &= ~(ICANON | ECHO);
+    /* apply the new settings */
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 }
 
 void terminalCleanup(){
+    /* apply settings from the `oldt` variable */
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
